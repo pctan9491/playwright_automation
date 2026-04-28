4d0089ee794d6a19f4f7f9b8b35a930d1bd23f11
Tan Ping Cheun <pingcheun.tan@MY.ifastfinancial.local>
2026-04-28 11:28:46 +0800

    Test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,3 +1,4 @@
+// @ts-ignore: Cannot find module '@playwright/test' or its corresponding type declarations.
 import { test, expect } from '@playwright/test';
 
 test.skip('has title', async ({ page }) => {
